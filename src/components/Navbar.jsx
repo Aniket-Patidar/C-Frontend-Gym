bd59d587cda7f4bbd35ce70599ef1fc3ba94611a
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-02-17 22:19:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,34 +51,33 @@ const Navbar = () => {
           >
             <ul className="flex flex-col font-semibold  text-lg text-white bg-transparent p-4 md:p-0 mt-4  border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
               <li>
-                <a
-                  href="#"
+                <Link href="#"
                   className="block py-2 px-3  rounded md:bg-transparent  md:p-0 "
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link 
                   href="#"
                   className="block py-2 px-3 rounded  md:hover:bg-transparent md:p-0 "
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 rounded md:hover:bg-transparent  md:p-0 "
                 >
                   Services
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3  rounded   md:p-0 ">
+                <Link href="#" className="block py-2 px-3  rounded   md:p-0 ">
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
