a56e530cb9298e8ca509e9bb6cdc93ef5f52414f
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-02-18 11:57:23 +0530

    add loader

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,14 +1,13 @@
 // components/LoadingPage.js
 
-import React from 'react';
-import styles from './LoadingPage.module.css';
+import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.loadingContent}>
-        <div className={styles.loadingText}>Loading...</div>
-        <div className={styles.loadingSpinner}></div>
+    <div className="loadingContainer">
+      <div className="loadingContent">
+        <div className="loadingText">Loading...</div>
+        <div className="loadingSpinner"></div>
       </div>
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,30 @@
 import CustomCursor from '@/components/Courser'
+import LoadingPage from '@/components/LoadingPage';
 import '@/styles/globals.css'
+import { Router } from 'next/router';
+import { useState } from 'react';
 
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <Component {...pageProps} />
-    <CustomCursor />
-  </>
+
+  const [loading, setLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', () => setLoading(true));
+  Router.events.on('routeChangeComplete', () => setLoading(false));
+  Router.events.on('routeChangeError', () => setLoading(false));
+
+
+  return (
+    <>
+      {loading ? ( // If loading is true, render LoadingPage
+        <LoadingPage />
+      ) : ( // If loading is false, render Component and CustomCursor
+        <>
+          <Component {...pageProps} />
+          <CustomCursor />
+        </>
+      )}
+    </>
+  );
+
 }
